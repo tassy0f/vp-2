b282de4df76590e36228f9da4c2fc344741bb230
Roman <7243180roma65@gmail.com>
2022-05-20 20:04:35 +0300

    # 9 (Начальная валидация формы)
    Добавлена стартовая валидация форма с помощью средств Ангуляра

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavComponent } from './components/shared/nav/nav.component';
 import { ShoppingCartsComponent } from './components/shopping-carts/shopping-carts.component';
 import { ProductItemComponent } from './components/shopping-carts/product-item/product-item.component';
 import { ModalWindowComponent } from './components/shopping-carts/modal-window/modal-window.component'
@@ -12,6 +9,7 @@ import { AngularYandexMapsModule } from 'angular8-yandex-maps';
 import { ModalFormComponent } from './components/shopping-carts/modal-window/modal-form/modal-form.component';
 import { SharedModule } from './components/shared/shared.module';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +23,8 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
     BrowserModule,
     AngularYandexMapsModule,
     SharedModule,
-    InfiniteScrollModule
+    InfiniteScrollModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/shopping-carts/modal-window/modal-form/modal-form.component.ts
+++ src/app/components/shopping-carts/modal-window/modal-form/modal-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalDialogService } from 'src/app/services/modal-dialog.service';
 
 @Component({
@@ -11,9 +12,22 @@ export class ModalFormComponent implements OnInit {
   constructor(private modalDialogService:ModalDialogService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      SecondName: new FormControl('', Validators.required),
+      Name: new FormControl('', Validators.required),
+      ThirdName: new FormControl(''),
+      Email: new FormControl('', [Validators.required, Validators.email]),
+      Phone_number: new FormControl('', Validators.required)
+    });
   }
+  
+  form!: FormGroup
 
   public closeModalForm() {
     this.modalDialogService.isShowModalForm = false
+  }
+
+  onSubmit() {
+    console.log('Submited!', this.form);
   }
 }
